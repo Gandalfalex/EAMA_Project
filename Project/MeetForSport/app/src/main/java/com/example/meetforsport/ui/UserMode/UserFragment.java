03344f8aadde51b17003ddff893b01cd98f4cb61
Gandalfalex <feragon98@gmx.de>
2021-12-03 12:40:07 +0100

    added empty project

---
+++ Project/MeetForSport/app/src/main/java/com/example/meetforsport/ui/UserMode/UserFragment.java
@@ -0,0 +1,45 @@
+package com.example.meetforsport.ui.UserMode;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.meetforsport.databinding.FragmentUserBinding;
+
+public class UserFragment extends Fragment {
+
+    private UserViewModel notificationsViewModel;
+    private FragmentUserBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        notificationsViewModel =
+                new ViewModelProvider(this).get(UserViewModel.class);
+
+        binding = FragmentUserBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textUserName;
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
