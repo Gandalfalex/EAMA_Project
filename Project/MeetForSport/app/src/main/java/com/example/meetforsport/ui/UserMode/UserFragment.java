1f0312ac3b732f9b89b39fadce9a832d9bc16a8a
Mattis Lahr <mlahr@techfak.uni-bielefeld.de>
2021-12-07 11:14:18 +0100

    added an adapter for the recyclerview  in the UserFragment with some dummy events

--- Project/MeetForSport/app/src/main/java/com/example/meetforsport/ui/UserMode/UserFragment.java
+++ Project/MeetForSport/app/src/main/java/com/example/meetforsport/ui/UserMode/UserFragment.java
@@ -1,6 +1,7 @@
 package com.example.meetforsport.ui.UserMode;
 
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +12,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.meetforsport.R;
 import com.example.meetforsport.databinding.FragmentUserBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserFragment extends Fragment {
 
@@ -34,6 +41,11 @@ public class UserFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        RecyclerView recyclerView = root.findViewById(R.id.event_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(new RecyclerViewAdapter(getContext(), DummyEvents()));
+
         return root;
     }
 
@@ -41,5 +53,17 @@ public class UserFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private static List<Pair<String,String>> DummyEvents(){
+
+        List<Pair<String,String>> myList = new ArrayList<>();
+        myList.add(new Pair<>("Football", "16:30, 8.12.21"));
+        myList.add(new Pair<>("Running", "12:00, 14.12.21"));
+        myList.add(new Pair<>("Basketball", "14:00, 24.12.21"));
+        myList.add(new Pair<>("Football", "12:00, 01.01.22"));
+        myList.add(new Pair<>("Football", "18:00, 07.01.22"));
+
+        return myList;
     }
 }
