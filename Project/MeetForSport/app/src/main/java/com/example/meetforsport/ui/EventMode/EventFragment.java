cfb73768250604327e5b3c29a9989ad91d6e4250
Mattis Lahr <mlahr@techfak.uni-bielefeld.de>
2021-12-12 16:36:08 +0100

    added recyclerViewAdapter for event list and buttons for possible filters and ordering

--- Project/MeetForSport/app/src/main/java/com/example/meetforsport/ui/EventMode/EventFragment.java
+++ Project/MeetForSport/app/src/main/java/com/example/meetforsport/ui/EventMode/EventFragment.java
@@ -4,17 +4,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -22,11 +28,13 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.meetforsport.R;
 import com.example.meetforsport.databinding.FragmentEventBinding;
-import com.example.meetforsport.ui.EventCreator.EventCreator;
 import com.example.meetforsport.ui.ServerCommunication.GetRequestCreator;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class EventFragment extends Fragment {
@@ -41,6 +49,8 @@ public class EventFragment extends Fragment {
         View root = binding.getRoot();
 
         View v = inflater.inflate(R.layout.fragment_event, container,false);
+        DrawerLayout filterDrawerLayout = (DrawerLayout) v.findViewById(R.id.filter_drawer);
+
         FloatingActionButton button = (FloatingActionButton) v.findViewById(R.id.fab_newEvent_EventMode);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +60,20 @@ public class EventFragment extends Fragment {
                 Log.d("Click", "CLICK..");
             }
         });
+
+        Button filterButton = v.findViewById(R.id.filter_button);
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("CLICK","CLICK");
+                filterDrawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
+
+        RecyclerView recyclerView = root.findViewById(R.id.events_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(new RecyclerViewAdapter(getContext(), DummyEvents()));
+
         return root;
     }
 
@@ -75,10 +99,17 @@ public class EventFragment extends Fragment {
         return  jsonObjectRequest;
     }
 
+    private static List<Pair<String,String>> DummyEvents(){
 
+        List<Pair<String,String>> myList = new ArrayList<>();
+        myList.add(new Pair<>("Football", "16:30, 8.12.21"));
+        myList.add(new Pair<>("Running", "12:00, 14.12.21"));
+        myList.add(new Pair<>("Basketball", "14:00, 24.12.21"));
+        myList.add(new Pair<>("Football", "12:00, 01.01.22"));
+        myList.add(new Pair<>("Football", "18:00, 07.01.22"));
 
-
-
+        return myList;
+    }
 
     @Override
     public void onDestroyView() {
