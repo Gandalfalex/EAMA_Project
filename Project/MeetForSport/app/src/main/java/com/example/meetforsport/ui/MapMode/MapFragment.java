a14ec5b7a3d485ad8c445aa4036801cd230d37d1
Gandalfalex <feragon98@gmx.de>
2021-12-16 12:42:16 +0100

    we both worked on the same file, differnet solutions

--- Project/MeetForSport/app/src/main/java/com/example/meetforsport/ui/MapMode/MapFragment.java
+++ Project/MeetForSport/app/src/main/java/com/example/meetforsport/ui/MapMode/MapFragment.java
@@ -1,17 +1,18 @@
 package com.example.meetforsport.ui.MapMode;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -19,21 +20,31 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.meetforsport.R;
 import com.example.meetforsport.databinding.FragmentMapBinding;
 import com.example.meetforsport.ui.EventCreator.EventCreator;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MapFragment extends Fragment implements View.OnClickListener {
+public class MapFragment extends Fragment implements View.OnClickListener, OnMapReadyCallback {
 
-    private MapViewModel homeViewModel;
+    private MapViewModel mapViewModel;
+    public MapView mapView;
     private FragmentMapBinding binding;
+    private GoogleMap googleMap;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel = new ViewModelProvider(this).get(MapViewModel.class);
+        mapViewModel = new ViewModelProvider(this).get(MapViewModel.class);
 
         binding = FragmentMapBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
@@ -41,17 +52,58 @@ public class MapFragment extends Fragment implements View.OnClickListener {
         });
 
 
-        View v = inflater.inflate(R.layout.fragment_map, container,false);
-
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
         FloatingActionButton button = (FloatingActionButton) v.findViewById(R.id.fab_newEvent_MapMode);
-
         button.setOnClickListener(this);
 
 
 
 
+
+        mapView = root.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        try {
+            MapsInitializer.initialize(getContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap gMap) {
+                googleMap = gMap;
+
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+                    //return;
+                }
+                else {Log.d("Permission", "Permission was granted");}
+                googleMap.setMyLocationEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34, 151);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
         return v;
     }
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mapView.onResume();
+
+    }
+
+
+
 
     @Override
     public void onDestroyView() {
@@ -65,7 +117,33 @@ public class MapFragment extends Fragment implements View.OnClickListener {
             case R.id.fab_newEvent_MapMode:
                 Intent intent = new Intent(getActivity() , EventCreator.class);
                 startActivity(intent);
-            break;
+                break;
         }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap gMap) {
+        googleMap = gMap;
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        else {Log.d("Permission", "Permission was not granted");}
+        googleMap.setMyLocationEnabled(true);
+
+        // For dropping a marker at a point on the Map
+        LatLng sydney = new LatLng(-34, 151);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+        // For zooming automatically to the location of the marker
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
